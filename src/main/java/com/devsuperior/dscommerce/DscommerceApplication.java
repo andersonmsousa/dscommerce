0d29f5f24b2644486014f214e92dbed77396835b
Anderson <anderson.msousa13@gmail.com>
2025-07-14 22:23:34 -0300

    ajustes config, role, user, uservice e seed

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscommerce;
 
-import com.devsuperior.dscommerce.entities.Category;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,9 +8,6 @@ public class DscommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
-		Category category1 = new Category();
-		Category category2 = new Category();
-		System.out.println(category1.equals(category2));
 	}
 
 }
--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -99,6 +99,13 @@ public class User implements UserDetails {
         roles.add(role);
     }
 
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) return true;
+        }
+        return false;
+    }
+
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
